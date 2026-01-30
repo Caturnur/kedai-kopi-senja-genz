1d32518ff3b2d7865c6dfe2f951c0c3d157e83d5
CATUR26\catur <catur.cahya@mii.co.id>
2026-01-30 19:27:36 +0700

    update kopi-senja

--- js/script.js
+++ js/script.js
@@ -1,18 +1,63 @@
-//toggle class active
+//toggle class active humberger menu & search form & shopping-cart
 
 const navbarNav = document.querySelector(".navbar-nav");
+const searchFrom = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+const shoppingCart = document.querySelector(".shopping-cart");
 
-//ketika menu hamburger diklik
+//ketika diklik akan active
 document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-//ketika klik diluar nav menu ilang
+document.querySelector("#search-button").onclick = (e) => {
+  searchFrom.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+};
 
-const hamburger = document.querySelector("#hamburger-menu");
+document.querySelector("#shopping-cart-button").onclick = () => {
+  shoppingCart.classList.toggle("active");
+};
+
+//ketika klik diluar nav hamburger & search ilang
+
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+const sc = document.querySelector("#shopping-cart-button");
 
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
+  if (!sb.contains(e.target) && !searchFrom.contains(e.target)) {
+    searchFrom.classList.remove("active");
+  }
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove("active");
+  }
 });
+
+//modal box
+
+const itemDetailModal = document.querySelector("#item-detail-modal");
+const itemDetailButton = document.querySelectorAll(".item-detail-button");
+const cm = document.querySelector(".modal .close-icons");
+
+itemDetailButton.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = "flex";
+    e.preventDefault();
+  };
+});
+
+cm.onclick = (e) => {
+  itemDetailModal.style.display = "none";
+  e.preventDefault();
+};
+
+window.onclick = (e) => {
+  if (e.target === itemDetailModal) {
+    itemDetailModal.style.display = "none";
+  }
+};
